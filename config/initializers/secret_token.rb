8bd2b424a0ec3008e33278efd078a123458c20a0
Derek LaPorte <derek@printedtime.com>
2013-05-02 11:43:03 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mytree::Application.config.secret_token = '1b64aef86f4617ae2a02078aca6b12e50b9262bb2af919677329189a476f28afef9ed457345180daefcbd85e05ab7ece6bd4962a1b26faecedcb63eba8d39773'
